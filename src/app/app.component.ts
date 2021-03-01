fcb0f7d4704fee010e8dcc0a24c5d991398cc3fc
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-03-01 18:16:01 +0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from "@angular/core";
+import { DemoErrorService } from "./demo-error.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  name = "Angular " + VERSION.major;
+
+  constructor(private errService: DemoErrorService) {}
+
+  ngOnInit(): void {
+    this.errService
+      .getHttpResponse()
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err),
+        () => console.log("Http Response completed")
+      );
+  }
 }
--- src/app/demo-error.service.ts
+++ src/app/demo-error.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
 
 @Injectable()
 export class DemoErrorService {
+  constructor() {}
 
-  constructor() { }
-
+  getHttpResponse(): Observable<Response> {
+    const error = new HttpErrorResponse({ status: 500 });
+    return throwError(error) as any;
+  }
 }
