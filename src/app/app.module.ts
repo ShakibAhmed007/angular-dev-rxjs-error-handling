b726f55f61845b43cb5af662b72395d005e96cc6
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-03-01 18:05:48 +0600

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { DemoErrorService } from "./demo-error.service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, DemoErrorComponent],
+  bootstrap: [AppComponent],
+  providers: [DemoErrorService]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/demo-error/demo-error.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-error',
+  templateUrl: './demo-error.component.html',
+  styleUrls: ['./demo-error.component.css']
+})
+export class DemoErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
